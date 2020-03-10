23798d9074f07d570a37ba301e9151ef93051287
132nd.Professor <Discord: 132nd.Professor>
2020-03-11 00:32:36 +0100

    removed superfluous license statements from individual files, set shebangs

--- common/frequency-table/generate.sh
+++ common/frequency-table/generate.sh
@@ -1,18 +1,4 @@
-# frequency-color_table: frequency color table for 132nd vWing as kneeboard 
-# Copyright © 2020 132nd.Professor
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#!/usr/bin/env zsh
 
 set -e
  
@@ -28,8 +14,8 @@ mkdir -p output
 
     latexmk -pdf 
     latexmk -c
-    gs -sDEVICE=png16m -r400 -o frequency_table_light_%d.png frequency_table_light.pdf 
-    gs -sDEVICE=png16m -r400 -o frequency_table_dark_%d.png frequency_table_dark.pdf 
+    gs -sDEVICE=png16m -r400 -o frequency_table_light_%d.png frequency_table_light.pdf
+    gs -sDEVICE=png16m -r400 -o frequency_table_dark-%d.png frequency_table_dark.pdf
     latexmk -C
 
     rm -f frequency_table_{light,dark}.tex frequencies.dat
